3c87159f74cfaa06d6dbc372976d78897ee5e818
Tom White <eugene.white@gmail.com>
2019-10-09 03:43:53 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,62 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.all.find(params[:id])
+    erb :'figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    end
+
+    @figure.save
+
+    erb :"/figures/show"
+  end
+
+  get '/figures/:id/edit' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    ####### allows removal of all associated landmarks and titles
+    if !params[:figure].keys.include?("landmark_ids")
+    params[:figure]["landmark_ids"] = []
+    end
+
+    if !params[:figure].keys.include?("title_ids")
+    params[:figure]["title_ids"] = []
+    end
+    #######
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    end
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
+    @figure.save
+    redirect "figures/#{@figure.id}"
+  end
+#
 end
